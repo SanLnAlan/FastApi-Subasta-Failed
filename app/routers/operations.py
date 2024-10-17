7e94bc5fb15b4e6f3425d15871405428bc8d2862
asandoval <asandoval@optimen.com.mx>
2024-10-16 23:41:26 -0600

    ejemplo de Body(...)

--- app/routers/operations.py
+++ app/routers/operations.py
@@ -1,14 +1,44 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Body
+from models import User, Ejemplo
+from typing import Optional
 
 router = APIRouter(
     prefix="/operations",
 )
 
-@router.get("/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@router.get("/")
+def read_operations():
+    return {}
+
+# def read_operation(operation_id: int):
+#     return {"item_id": operation_id}
 
 # @router.put("/{item_id}")
 # def update_operation(
     
 # )
+
+
+
+
+@router.post("/{operation_id}")
+async def create_operation(
+    *, #check this
+    operation_id: int = Path(
+        ...,
+        title="Id of the operation to update",
+        description="This is the description",
+        ge=0
+        ),
+    userOperator: User,
+    amount_offered: Optional[float] = 0.0,
+    ejemplo: int = Body(...)
+
+):
+    results = {"operation_id": operation_id}
+    results.update({"user": userOperator})
+    if amount_offered:
+       results.update({"amount_offered": amount_offered})
+    if ejemplo:
+        results.update({"ejemplo": ejemplo})
+    return results
